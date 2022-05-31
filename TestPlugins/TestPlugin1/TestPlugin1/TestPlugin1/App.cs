84e5af08d1f2b6460637e1397ea0f6fd0e0f5106
Shiva TEClab <shivajreddy@outlook.com>
2022-05-31 11:29:00 -0400

    Hello world command.cs and App.cs finished along with resource image

--- TestPlugins/TestPlugin1/TestPlugin1/TestPlugin1/App.cs
+++ TestPlugins/TestPlugin1/TestPlugin1/TestPlugin1/App.cs
@@ -5,21 +5,103 @@ using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using System;
 using System.Collections.Generic;
-
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+using System.IO;
+using Result = Autodesk.Revit.UI.Result;
 #endregion
 
 namespace TestPlugin1
 {
-    internal class App : IExternalApplication
+    public class App : IExternalApplication
     {
-        public Result OnStartup(UIControlledApplication a)
+
+        public Result OnShutdown(UIControlledApplication application)
         {
             return Result.Succeeded;
         }
 
-        public Result OnShutdown(UIControlledApplication a)
+        public Result OnStartup(UIControlledApplication application)
         {
+            // create the ribbon panel
+            RibbonPanel panel = CreateRibbonPanel(application);
+
+            // Get the assembly path. This is the path of the folder that the application is being compiled to.
+            string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+
+            const string buttonName = "button1";
+            const string buttonText = "button1Text";
+            const string buttonToolTip = "Test tool tip for button1";
+
+            // declaring the panel.AddItem() as PushButton
+
+            PushButtonData pushButtonData = new PushButtonData(buttonName, buttonText, thisAssemblyPath, "TestPlugin1.Command");
+
+            var pushButton = panel.AddItem(pushButtonData);
+            if (pushButton is PushButton button)
+            {
+                button.ToolTip = buttonToolTip;
+                Uri uri = new Uri(Path.Combine((Path.GetDirectoryName(thisAssemblyPath)), "Resource", "button1.png"));
+                BitmapImage bitmapImage = new BitmapImage(uri);
+                button.LargeImage = bitmapImage;
+            }
+
+            // declaring the panel.AddItem() as PushButton
+            //if (panel.AddItem(new PushButtonData(buttonName, buttonText, thisAssemblyPath, "TestPlugin1.Command")) is PushButton button)
+            //{
+            //    button.ToolTip = buttonToolTip;
+            //    Uri uri = new Uri(Path.Combine((Path.GetDirectoryName(thisAssemblyPath)), "Resource", "button1.png"));
+            //    BitmapImage bitmapImage = new BitmapImage(uri);
+            //    button.LargeImage = bitmapImage;
+            //}
             return Result.Succeeded;
         }
+
+        // Method to create the Ribbon panel for the given UIControlledApplication instance
+        public RibbonPanel CreateRibbonPanel(UIControlledApplication app)
+        {
+
+            // constant names for the panel, and the tabs inside the panel
+            const string panelName = "Panel1";
+            const string tab1Name = "Tab1";
+            RibbonPanel ribbonPanel = null;
+
+            // Create the tab inside the 'app' instance
+            try
+            {
+                app.CreateRibbonTab(tab1Name);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Couldn't create the Ribbon Panel" + e);
+            }
+
+            // Create the panel with the created tab
+            try
+            {
+                app.CreateRibbonPanel(tab1Name, panelName);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            // Create list of all panels -> for in the 'tabName' of 'app'
+            List<RibbonPanel> panels = app.GetRibbonPanels(tab1Name);
+            // Get only the panel we own. Shouldn't add tabs to other panels
+            foreach (var panel in panels.Where(panel => panel.Name == panelName))
+            {
+                ribbonPanel = panel;
+            }
+
+            return ribbonPanel;
+        }
+
+
+
     }
 }
--- TestPlugins/TestPlugin1/TestPlugin1/TestPlugin1/Command.cs
+++ TestPlugins/TestPlugin1/TestPlugin1/TestPlugin1/Command.cs
@@ -15,44 +15,58 @@ namespace TestPlugin1
     [Transaction(TransactionMode.Manual)]
     public class Command : IExternalCommand
     {
+
         public Result Execute(
-          ExternalCommandData commandData,
-          ref string message,
-          ElementSet elements)
+            ExternalCommandData commandData,
+            ref string message,
+            ElementSet elements)
         {
-            UIApplication uiapp = commandData.Application;
-            UIDocument uidoc = uiapp.ActiveUIDocument;
-            Application app = uiapp.Application;
-            Document doc = uidoc.Document;
 
-            // Access current selection
 
-            Selection sel = uidoc.Selection;
+            TaskDialog.Show("Hello World!", "Hello world :) ??");
 
-            // Retrieve elements from database
-
-            FilteredElementCollector col
-              = new FilteredElementCollector(doc)
-                .WhereElementIsNotElementType()
-                .OfCategory(BuiltInCategory.INVALID)
-                .OfClass(typeof(Wall));
-
-            // Filtered element collector is iterable
-
-            foreach (Element e in col)
-            {
-                Debug.Print(e.Name);
-            }
-
-            // Modify document within a transaction
-
-            using (Transaction tx = new Transaction(doc))
-            {
-                tx.Start("Transaction Name");
-                tx.Commit();
-            }
 
             return Result.Succeeded;
         }
+
+        //public Result Execute(
+        //  ExternalCommandData commandData,
+        //  ref string message,
+        //  ElementSet elements)
+        //{
+        //    UIApplication uiapp = commandData.Application;
+        //    UIDocument uidoc = uiapp.ActiveUIDocument;
+        //    Application app = uiapp.Application;
+        //    Document doc = uidoc.Document;
+
+        //    // Access current selection
+
+        //    Selection sel = uidoc.Selection;
+
+        //    // Retrieve elements from database
+
+        //    FilteredElementCollector col
+        //      = new FilteredElementCollector(doc)
+        //        .WhereElementIsNotElementType()
+        //        .OfCategory(BuiltInCategory.INVALID)
+        //        .OfClass(typeof(Wall));
+
+        //    // Filtered element collector is iterable
+
+        //    foreach (Element e in col)
+        //    {
+        //        Debug.Print(e.Name);
+        //    }
+
+        //    // Modify document within a transaction
+
+        //    using (Transaction tx = new Transaction(doc))
+        //    {
+        //        tx.Start("Transaction Name");
+        //        tx.Commit();
+        //    }
+
+        //    return Result.Succeeded;
+        //}
     }
 }
